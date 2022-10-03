eebdffb43f57e5daaddc0972862b64ecf5c18e9a
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-10-03 23:38:07 +0530

    Added: Check duplicate entries in Addressbook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -16,22 +16,5 @@ public class AddressBook {
     public void setAddressbook(ArrayList<Contact> addressbook) {
         this.addressbook = addressbook;
     }
-     /*
-        Printing method to print all the contacts in the list using for each loop
-         */
 
-    public void printAddressBook() {
-        for (Contact contact : this.addressbook) {
-
-            System.out.println("-----------------------------------------------------");
-            System.out.println("First name = " + contact.getFirstName());
-            System.out.println("last name = " + contact.getLastName());
-            System.out.println("address = " + contact.getEmail());
-            System.out.println("city = " + contact.getCity());
-            System.out.println("state = " + contact.getState());
-            System.out.println("zip = " + contact.getZipCode());
-            System.out.println("phone number = " + contact.getMobileNumber());
-            System.out.println("-----------------------------------------------------");
-        }
-    }
 }
--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,7 +1,6 @@
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class AddressBookMain {
@@ -9,7 +8,7 @@ public class AddressBookMain {
     public static Contact getInput() {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Add details of a person you want to add");
+        System.out.println("Add details of a person you want to edit");
 
         System.out.println("Please provide first name");
         String firstName = scanner.next();
@@ -42,62 +41,38 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
 
-    /*
+        AddressBook addressBook = new AddressBook();
+        Contact contact = getInput();
+     /*
     Adding Contacts details using Constructor of person class
     */
         Contact person1 = new Contact("amit", "dutt", "8165116516", "amit123@gmail.com", "kaithal", "Haryana", 84516548);
         Contact person2 = new Contact("ankita", "sharma", "49816515649", "ankita123@gmail.com", "mumbai", "haryana", 6558498);
-        Contact person3 = new Contact("sumit", "kumar", "9146516549", "sumit123@gmail.com", "pune", "maharashtra", 849840);
+        Contact person3 = new Contact("sumit", "kumar", "9146516549", "sumit123@gmail.com", "gurgaon", "maharashtra", 849840);
         Contact person4 = new Contact("anybody", "rana", "919848949848", "asdsa@gmail.com", "gurgaon", "haryana", 482702);
         Contact person5 = new Contact("anita", "sharma", "9119548989489", "anitasharma@gmail.com", "delhi", "delhi", 2554654);
 
-    /*
-    Adding contacts to different address books by using addContact method means every single addressbook
-    has multiple contacts
-    */
-        AddressBook addressBook1 = new AddressBook();
-        addressBook1.addContact(person1);
-        addressBook1.addContact(person2);
-        AddressBook addressBook2 = new AddressBook();
-        addressBook2.addContact(person3);
-        addressBook2.addContact(person4);
-        AddressBook addressBook3 = new AddressBook();
-        addressBook3.addContact(person5);
-        addressBook3.addContact(person3);
-        addressBook3.addContact(person1);
-        AddressBook addressBook4 = new AddressBook();
-        addressBook4.addContact(getInput());
-        addressBook4.addContact(getInput());
 
+        addressBook.addContact(person1);
+        addressBook.addContact(person2);
+        addressBook.addContact(person3);
+        addressBook.addContact(person4);
     /*
-    Creating hashmap of keys String type and value of addressbook
-    */
-
-        Map<String, AddressBook> map = new HashMap<>();
-    /*
-    putting different address books in the map. Map has two parts first one is key
-    and Second one is value
-    */
-
-        map.put("addressBook1", addressBook1);
-        map.put("addressBook2", addressBook2);
-        map.put("addressBook3", addressBook3);
-        map.put("addressBook4", addressBook4);
-
-    /*
-    for-each loop to print keys and values. e.getkey() to print keys and getting values using calling print function of
-    address book we can use toString also to override the memory otherwise it shows memory location of address book only.
-    by using printAddressBook function it shows every detail of the contact.
+    Java Stream to check duplicate entry and add a contact if contact is not present by filtering the contact
+    with firstname and last name of the address book which is declared in equals and hashcode method
+    in Contact class.
      */
 
-        for (Map.Entry<String, AddressBook> e : map.entrySet()) {
-            System.out.println(e.getKey());
-            e.getValue().printAddressBook();
 
-        }
-
+        addressBook.getAddressbook().stream().filter(a -> a.equals(contact)).findAny().orElseGet(() -> {
+            addressBook.getAddressbook().add(contact);
+            return contact;
+        });
+    /*
+    Printing all the contacts using for-each loop
+    */
+        addressBook.getAddressbook().stream().forEach(x -> System.out.println(x));
 
     }
-
 
 }
